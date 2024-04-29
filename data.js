555da4f49b5dd6d7b827bb6b3d987992a9ad24b2
Jeanne Viallard <viallard.jeanne@gmail.com>
2024-04-29 09:11:00 +0200

    Add homepage carrousel functionality

---
+++ data.js
@@ -0,0 +1,22 @@
+export const homepageCarrouselData = [
+  {
+    img: "image-hero-paramour-1110.jpg",
+    title: "Project <span>Paramour</span>",
+    des: "Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.",
+  },
+  {
+    img: "image-hero-seraph-1110.jpg",
+    title: "Seraph<span>Station</span>",
+    des: "The Seraph Station project challenged us to design a unique station that would transport people through time. The result is a fresh and futuristic model inspired by space stations.",
+  },
+  {
+    img: "image-hero-federal-1110.jpg",
+    title: "Federal II<span>Tower</span>",
+    des: "A sequel theme project for a tower originally built in the 1800s. We achieved this with a striking look of brutal minimalism with modern touches.",
+  },
+  {
+    img: "image-hero-trinity-1110.jpg",
+    title: "Trinity Bank <span>Tower</span>",
+    des: "Trinity Bank challenged us to make a concept for a 84 story building located in the middle of a city with a high earthquake frequency. For this project we used curves to blend design and stability to meet our objectives.",
+  },
+];
--- index.js
+++ index.js
@@ -1,7 +1,39 @@
+// MOBILE NAV TOGGLE
+
 document
   .getElementById("open-menu-btn")
   .addEventListener("click", toggleMobileMenu);
 
 function toggleMobileMenu() {
   document.getElementById("mobile-nav").classList.toggle("mobile-nav--hidden");
+}
+
+// HOMEPAGE CARROUSEL
+
+import { homepageCarrouselData } from "./data.js";
+
+const controlsArray = Array.from(document.getElementsByClassName("control"));
+
+const homepageCarrousel = document.getElementById("homepage-carrousel");
+const homepageCarrouselTitle = document.getElementById(
+  "homepage-carrousel__title"
+);
+const homepageCarrouselDes = document.getElementById("homepage-carrousel__des");
+
+controlsArray.forEach((control, index) => {
+  control.addEventListener("click", () => changeCarrouselSlide(index));
+});
+
+function changeCarrouselSlide(index) {
+  const currentItem = homepageCarrouselData[index];
+  homepageCarrousel.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.35)), 
+  url(images/${currentItem.img})`;
+
+  homepageCarrouselTitle.innerHTML = currentItem.title;
+  homepageCarrouselDes.textContent = currentItem.des;
+
+  controlsArray.forEach((control) => {
+    control.classList.remove("control--current");
+  });
+  controlsArray[index].classList.add("control--current");
 }
